ceadd3d33e7e4b3b067bc1d4eda34d1021798e5b
Dzides <idzigun@.ru>
2020-07-07 18:04:13 +0300

    added header

--- gulpfile.js
+++ gulpfile.js
@@ -8,8 +8,8 @@ let gulp = require('gulp'),
    cssmin = require('gulp-cssmin');
 
 gulp.task('sass', function (){
-   return gulp.src('app/scss/style.scss')
-      .pipe(sass({outputStyle: 'compressed'}))
+   return gulp.src('app/scss/**/*.scss')
+      .pipe(sass({outputStyle: 'expanded'}))
       .pipe(rename({suffix: '.min'}))
       .pipe(autoprefixer({
          overrideBrowserslist: ['last 8 versions']
@@ -58,7 +58,7 @@ gulp.task('browser-sync', function(){
 });
 
 gulp.task('watch', function (){
-   gulp.watch('app/scss/style.scss', gulp.parallel('sass'))
+   gulp.watch('app/scss/**/*.scss', gulp.parallel('sass'))
    gulp.watch('app/*.html', gulp.parallel('html'))
    gulp.watch('app/js/*.js', gulp.parallel('js'))
 });
