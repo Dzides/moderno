13713d0eca26a7634f81e872c0bfc9e7eb60e4d6
Dzides <idzigun@.ru>
2020-07-08 18:35:06 +0300

    added product-slider and product

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,20 @@
-$(function(){
+$(function (){
 
 
-    var mixer = mixitup('.products__inner-box');    
+    $(".rate-star").rateYo({
+        rating: 5,
+        starWidth: "12px",
+        readOnly: true
+    });
+
+    $('.product-slider__inner').slick({
+        dots: true,
+        arrows: false,
+        slidesToShow: 4,
+        slidesToScroll: 4
+    });
+
+    var mixer = mixitup('.products__inner-box');
 
 
 
