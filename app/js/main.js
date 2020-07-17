8f8f03702ffc7edba83d26a4e2b0d59d61f6fce6
Dzides <idzigun@.ru>
2020-07-17 23:38:17 +0300

    add adaptiv header

--- app/js/main.js
+++ app/js/main.js
@@ -48,9 +48,15 @@ $(function (){
         $('.icon-th-list').removeClass('active');
     })
 
+    $('.menu__btn').on('click', function(){
+        $('.menu__list').slideToggle();
+    });
+    
+    $('.header__btn-menu').on('click', function(){
+        $('.header__box').toggleClass('active');
+    });
+
+
     var mixer = mixitup('.products__inner-box');
-
-
-
 
 });
