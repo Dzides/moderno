19aa5401a202034add2e786824da8f5fce2a5d64
Dzides <idzigun@.ru>
2020-07-20 23:56:49 +0300

    add Upload Page

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,4 @@
-$(function (){
+$(function () {
 
 
     $(".rate-star").rateYo({
@@ -14,7 +14,7 @@ $(function (){
         slidesToScroll: 4
     });
 
-    
+
     $(".js-range-slider").ionRangeSlider({
         type: "double",
         grid: false,
@@ -37,31 +37,33 @@ $(function (){
         // block: false  
     });
 
-    $('.icon-th-list').on('click', function(){
+    $('.icon-th-list').on('click', function () {
         $('.product__item').addClass('list');
         $('.icon-th-list').addClass('active');
         $('.icon-th-large').removeClass('active');
     });
-    $('.icon-th-large').on('click', function(){
+    $('.icon-th-large').on('click', function () {
         $('.product__item').removeClass('list');
         $('.icon-th-large').addClass('active');
         $('.icon-th-list').removeClass('active');
     })
 
-    $('.product-one__tabs .tab, settings__tabs .tab').on('click', function(event) {
+    $('.product-one__tabs .tab, settings__tabs .tab').on('click', function (event) {
         var id = $(this).attr('data-id');
-            $('.product-one__tabs, settings__tabs').find('.tab-item').removeClass('active-tab').hide();
-            $('.product-one__tabs .tabs, settings__tabs .tabs').find('.tab').removeClass('active');
-            $(this).addClass('active');
-            $('#'+id).addClass('active-tab').fadeIn();
-            return false;
-        });
+        $('.product-one__tabs, settings__tabs').find('.tab-item').removeClass('active-tab').hide();
+        $('.product-one__tabs .tabs, settings__tabs .tabs').find('.tab').removeClass('active');
+        $(this).addClass('active');
+        $('#' + id).addClass('active-tab').fadeIn();
+        return false;
+    });
 
-    $('.menu__btn').on('click', function(){
+    $('input[type="file"], select').styler();
+
+    $('.menu__btn').on('click', function () {
         $('.menu__list').slideToggle();
     });
-    
-    $('.header__btn-menu').on('click', function(){
+
+    $('.header__btn-menu').on('click', function () {
         $('.header__box').toggleClass('active');
     });
 
