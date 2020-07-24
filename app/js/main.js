fc09e963dc5cb2e942f7dbb6ee51d6aa721afd30
Dzides <idzigun@.ru>
2020-07-25 01:40:04 +0300

    end adaptiv

--- app/js/main.js
+++ app/js/main.js
@@ -11,7 +11,33 @@ $(function () {
         dots: true,
         arrows: false,
         slidesToShow: 4,
-        slidesToScroll: 4
+        slidesToScroll: 4,
+        responsive: [
+            {
+                breakpoint: 1900,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 1441,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 801,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    dots: false,
+                }
+            },
+        ]
     });
 
 
